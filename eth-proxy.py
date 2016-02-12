9228b6ea19accab626cc1ff67bc356179c8a6be5
Sling00 <sling00@gmail.com>
2016-02-11 22:13:42 -0600

    test 2
    test 2

--- eth-proxy.py
+++ eth-proxy.py
@@ -4,7 +4,8 @@
 import time
 import os
 import socket
-
+import re
+import string
 from stratum import settings
 from stratum import ethos
 import stratum.logger
@@ -28,6 +29,39 @@ from mining_libs import client_service
 from mining_libs import jobs
 from mining_libs import version
 from mining_libs.jobs import Job
+
+
+
+ethosconfig = open("/home/ethos/local", "r")
+def setup ():
+    for line in ethosconfig:
+        if re.match("(.*)(?<=proxypool1 )(.*)", line):
+            proxypool1 = line.rstrip('\n').split(" ", 2)[1].split(":", 2)
+#        myvar = line.split(" ", 2)
+#       print line,
+#       print myvar
+#       print myvar2,
+#       myvar3 = myvar[1].split(":", 2)
+
+            mainpool = proxypool1[0]
+            mainport = proxypool1[1]
+#       yield mainpool
+#            print mainpool
+#.rstrip('\n')
+#            print pool
+#            print port
+        elif re.match("(.*)(?<=proxypool2 )(.*)", line):
+            proxypool2 = line.rstrip('\n').split(" ", 2)[1].split(":", 2)
+        backuppool = proxypool2[0]
+            backupport = proxypool2[1]
+#        print backupport
+#        print backuppool
+#        elif re.match("(.*)(?<=proxywallet )(.*)", line):
+#            proxywallet = line.rstrip('\n').split(" ", 2)[1]
+#    yield (mainpool, mainport, backuppool, backupport, proxywallet)
+#        print proxywallet
+
+setup()
 
 def on_shutdown(f):
     '''Clean environment properly'''
